e464b1e1b8c2dbaed49cc51fb47e95795135ba91
CALDEJOS <30401866+joscompu@users.noreply.github.com>
2020-05-06 12:55:22 -0300

    Initial commit

---
+++ src/main/java/com/spring/boot/SpringBootJpaAndH2Application.java
@@ -0,0 +1,13 @@
+package com.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaAndH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaAndH2Application.class, args);
+	}
+
+}
